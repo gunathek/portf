be4f14f8bfb4448dc4df794a7246c56b69e0f018
guna-101 <guna.thek@respo.co.in>
2025-05-22 00:21:12 +0530

    customised particles

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,9 @@ export default function Home() {
               {/*<NavBarDemo/>*/}
               <Particles
                   className="absolute inset-0 -z-10 animate-fade-in"
-                  quantity={100}
+                  quantity={400}
+                  staticity={30}
+                  size={0.5}
               />
               <div
                   className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"/>
@@ -62,7 +64,7 @@ export default function Home() {
                   className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"/>
               <div className="my-16 text-center animate-fade-in">
                   <h2 className="text-sm text-zinc-500 ">
-                      I'm building{" "}
+                      Building{" "}
                       <Link
                           target="_blank"
                           href="https://smolboards.com"
@@ -222,24 +224,3 @@ const DATA = {
 //     );
 // }
 
-// export function ParticlesDemo() {
-//     const {theme} = useTheme();
-//     const [color, setColor] = useState("#ffffff");
-//
-//     useEffect(() => {
-//         setColor(theme === "dark" ? "#ffffff" : "#000000");
-//     }, [theme]);
-//
-//     return (
-//         <div
-//             className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-//             <Particles
-//                 className="absolute inset-0"
-//                 quantity={100}
-//                 ease={80}
-//                 color={color}
-//                 refresh
-//             />
-//         </div>
-//     );
-// }
