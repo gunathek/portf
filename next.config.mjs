1c41d10b8ecb60d75359735571e57442f8acb598
guna-101 <guna.thek@respo.co.in>
2025-06-10 15:51:09 +0530

    coming_soon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/projects',
+        destination: '/coming-soon',
+        permanent: false,
+      },
+      {
+        source: '/about',
+        destination: '/coming-soon',
+        permanent: false,
+      },
+      {
+        source: '/skills',
+        destination: '/coming-soon',
+        permanent: false,
+      },
+      {
+        source: '/work',
+        destination: '/coming-soon',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
